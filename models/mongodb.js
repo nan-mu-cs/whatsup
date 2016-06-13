46e0c611b0653e1c802573131451e81b37762b26
andyyang <3130000495@zju.edu.cn>
2016-06-13 23:43:39 +0800

    change mongo config

--- models/mongodb.js
+++ models/mongodb.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
-//mongoose.connect('mongodb://'+process.env.MONGO_PORT_27017_TCP_ADDR+':'+process.env.MONGO_PORT_27017_TCP_PORT+'/whatsup');
-mongoose.connect('mongodb://localhost/whatsup');
+mongoose.connect('mongodb://'+process.env.MONGO_PORT_27017_TCP_ADDR+':'+process.env.MONGO_PORT_27017_TCP_PORT+'/whatsup');
+//mongoose.connect('mongodb://localhost/whatsup');
 
 var db = mongoose.connection;
 
